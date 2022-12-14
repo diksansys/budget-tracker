c00adcfefd2993197e08b10b87ad2f7271925525
diksansys <119845431+diksansys@users.noreply.github.com>
2022-12-14 11:39:01 +0530

    navigation event added

--- assets/js/Events/navigation.js
+++ assets/js/Events/navigation.js
@@ -1,15 +1,22 @@
 $(document).ready(() => {
-    $(".profileFootLink").click(() => {
-        $(".nav-link").removeClass('active');
-        $(".profileFootLink").addClass("active");
-        $(".main-section").hide();
-        $(".user-section").show();
-    })
-
     $(".homeFootLink").click(() => {
         $(".nav-link").removeClass('active');
         $(".homeFootLink").addClass("active");
-        $(".user-section").hide();
+        $(".main-page").hide();
         $(".main-section").show();
+    })
+
+    $(".payrollFootLink").click(() => {
+        $(".nav-link").removeClass('active');
+        $(".payrollFootLink").addClass("active");
+        $(".main-page").hide();
+        $(".payroll-section").show();
+    })
+
+    $(".profileFootLink").click(() => {
+        $(".nav-link").removeClass('active');
+        $(".profileFootLink").addClass("active");
+        $(".main-page").hide();
+        $(".user-section").show();
     })
 })
